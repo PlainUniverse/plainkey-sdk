31c5fbe09933454c8142be478f7939ccab73ee5c
ehs5 <espenhs@gmail.com>
2025-11-14 21:33:31 +0100

    New simpler approach on calling class methods in Vue composable.

--- packages/vue/dist/index.js
+++ packages/vue/dist/index.js
@@ -13,10 +13,39 @@ import { PlainKey } from "@plainkey/browser";
 */
 function usePlainKey(projectId, baseUrl) {
 	const plainKey = new PlainKey(projectId, baseUrl);
+	/**
+	* Authenticates a user. Can be used for login, verification, 2FA, etc.
+	* Will require user interaction to authenticate.
+	*
+	* @param userIdentifier - An object with either the user's PlainKey User ID or their userName.
+	*/
+	function authenticate(userIdentifier) {
+		return plainKey.authenticate(userIdentifier);
+	}
+	/**
+	* Registration of a new user with a passkey. Will require user interaction to create a passkey.
+	*
+	* @param userName - A stable unique identifier for the user, like an email address or username.
+	* Can be empty for usernameless login.
+	*/
+	function createUserWithPasskey(userName) {
+		return plainKey.createUserWithPasskey(userName);
+	}
+	/**
+	* Adds a passkey to an existing user. Will require user interaction to create a passkey.
+	*
+	* @param userToken - The user authentication token, obtained from login.
+	* Do NOT store it in local storage, database, etc. Always keep it in memory.
+	*
+	* @param userIdentifier - An object with either the user's PlainKey User ID or their userName.
+	*/
+	function addPasskey(userToken, userIdentifier) {
+		return plainKey.addPasskey(userToken, userIdentifier);
+	}
 	return {
-		authenticate: plainKey.authenticate.bind(plainKey),
-		createUserWithPasskey: plainKey.createUserWithPasskey.bind(plainKey),
-		addPasskey: plainKey.addPasskey.bind(plainKey)
+		authenticate,
+		createUserWithPasskey,
+		addPasskey
 	};
 }
 
--- packages/vue/src/usePlainkey/index.ts
+++ packages/vue/src/usePlainkey/index.ts
@@ -14,10 +14,41 @@ import { UserIdentifier } from "@plainkey/types"
 export function usePlainKey(projectId: string, baseUrl?: string) {
   const plainKey = new PlainKey(projectId, baseUrl)
 
-  // .bind() attaches the class method to the plainKey instance so it can be used as a standalone function.
+  /**
+   * Authenticates a user. Can be used for login, verification, 2FA, etc.
+   * Will require user interaction to authenticate.
+   *
+   * @param userIdentifier - An object with either the user's PlainKey User ID or their userName.
+   */
+  function authenticate(userIdentifier: UserIdentifier) {
+    return plainKey.authenticate(userIdentifier)
+  }
+
+  /**
+   * Registration of a new user with a passkey. Will require user interaction to create a passkey.
+   *
+   * @param userName - A stable unique identifier for the user, like an email address or username.
+   * Can be empty for usernameless login.
+   */
+  function createUserWithPasskey(userName?: string) {
+    return plainKey.createUserWithPasskey(userName)
+  }
+
+  /**
+   * Adds a passkey to an existing user. Will require user interaction to create a passkey.
+   *
+   * @param userToken - The user authentication token, obtained from login.
+   * Do NOT store it in local storage, database, etc. Always keep it in memory.
+   *
+   * @param userIdentifier - An object with either the user's PlainKey User ID or their userName.
+   */
+  function addPasskey(userToken: string, userIdentifier: UserIdentifier) {
+    return plainKey.addPasskey(userToken, userIdentifier)
+  }
+
   return {
-    authenticate: plainKey.authenticate.bind(plainKey),
-    createUserWithPasskey: plainKey.createUserWithPasskey.bind(plainKey),
-    addPasskey: plainKey.addPasskey.bind(plainKey)
+    authenticate,
+    createUserWithPasskey,
+    addPasskey
   }
 }
